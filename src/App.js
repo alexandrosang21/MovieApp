e2d7fe82d15757e1f66d19b2427c2a835ae9fd0e
alexandrosang21 <R%#Me51x3VO5>
2022-10-06 00:59:54 +0300

    initial setup - testing app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+function Person(props) {
+  return (
+    <>
+      <h2>Hello {props.name}</h2>
+    </>
+  );
+}
 
 function App() {
+  const isUserLoggedIn = true;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isUserLoggedIn ? (
+          <>
+            <h1>You are logged in!</h1>
+            <Person name="John" />
+            <Person />
+          </>
+        ) : (
+          <>
+            <h1>not logged in</h1>
+          </>
+        )}
       </header>
     </div>
   );
